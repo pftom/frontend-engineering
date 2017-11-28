1985550e592051295b25be32be6dd96c435cef42
黄巍 <huan1043269994@gmail.com>
2017-11-28 17:15:15 +0800

    [ADD] add npm_learning package, and test it.

--- ESLint/eslint_learning/src/index.js
+++ ESLint/eslint_learning/src/index.js
@@ -1,8 +1,3 @@
-const a = 3;
-const b = 2;
+const npmLearning = require('npm_learning');
 
-function myFunc(a1, b1) {
-  return a1 + b1;
-}
-
-myFunc(a, b);
+npmLearning.printMsg();
--- NPM/npm_learning/index.js
+++ NPM/npm_learning/index.js
@@ -1,4 +1,3 @@
-var lodash = require('lodash');
-
-var output = lodash.without([1, 2, 3], 1);
-console.log(output);
+exports.printMsg = function () {
+  console.log('This is a message from the demo package');
+};
